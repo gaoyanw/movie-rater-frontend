8ad03745300e264130595f6fb37d31925cef4824
Gaoyan Wang <gaoyanw68@gmail.com>
2022-12-11 08:01:57 -0800

    58. Simple component

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <h2> Movie Rater</h2>
+</template>
+
+<script>
+    export default{
+        name: "MovieRaterHeader"
+    }
+</script>
+
+<style scoped>
+    h1, h2{
+        color: red
+    }
+</style>
